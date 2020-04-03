acc6976a351fb3c8def92fe669a87b1241a3e24c
Dev Vixx <iroka.victor@yahoo.com>
2020-04-03 12:45:25 -0400

    made some changes on the db connection because of the change to env_mode and deployed to heroku and i coded the db connection string

--- db_connection.js
+++ db_connection.js
@@ -1,12 +1,18 @@
 const { Sequelize } = require('sequelize')
 
+
+// i displayed the db connection cause it wont 
+// connect using node env
 const sequelize = new Sequelize(
-  process.env.db /* db name */,
-  process.env.USER /* db username*/,
-  process.env.PWD /*db user password */,
+  "heroku_78a1def128e7043",
+  "bc42f0b6e62d34",
+  'dd1f74e6',
+  // process.env.db /* db name */,
+  // process.env.USER /* db username*/,
+  // process.env.PWD /*db user password */,
   {
     dialect: 'mysql',
-    host: process.env.DB_HOST
+    host:  'us-cdbr-iron-east-01.cleardb.net'
   }
 )
 
